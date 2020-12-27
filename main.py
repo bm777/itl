6642ac9979368203b5d10a2252319cae686db01b
Bayangmbe Mounmo <bayangp0@gmail.com>
2020-12-27 17:18:24 +0100

    Added template for cours

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# home page for 127.0.0.1:5000
+# 127.0.0.1 Local ip address
+# 5000 is default port adress
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+# home page for 127.0.0.1:5000/cours
+# 127.0.0.1 Local ip address
+# 5000 is default port adress
+@app.route('/cours/<name>')
+def cours(name=None):
+
+	tmp = name if name is not None else "None"
+	return render_template("cours.html", name=tmp)
